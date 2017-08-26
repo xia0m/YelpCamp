155151a3230ac83ef0215b7507e7d202160c798a
xia0m <xia0m@Zemins-MacBook-Pro.local>
2017-08-26 01:32:09 -0700

    final tweak

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(3000, function () {
+app.listen(process.env.PORT||3000, function () {
     console.log("The YelpCamp loaded");
 });
 
