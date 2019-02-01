020d20dc0c0b3cd172deed121ae16c8541ad45ec
xia0m <xia0m@Zemins-MacBook-Pro.local>
2019-02-01 12:27:53 -0800

    fix minor error in index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.post("/register",(req,res)=>{
             return res.redirect("/register");
         }
         passport.authenticate("local")(req,res,function(){
-            req.flash("success","Welcome to YelpCamp" + user.username);
+            req.flash("success","Welcome to YelpCamp " + user.username);
             res.redirect("/campgrounds");
         });
     });
